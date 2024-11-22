96dd831349cb3e4875de59fc78c07694b40a28d0
LaryssaGabi <lary.gabriela85@gmail.com>
2024-11-21 23:09:29 -0300

    first commit

---
+++ src/main/java/com/rocketseat/redirectUrlShortner/Main.java
@@ -0,0 +1,58 @@
+package com.rocketseat.redirectUrlShortner;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+
+    private final S3Client s3Client = S3Client.builder().build();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+        Map<String, Object> response = new HashMap<>();
+        String pathParameters = (String) input.get("rawPath");
+
+        if (pathParameters == null || pathParameters.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input: 'rawPath' is required.");
+        }
+
+        String shortUrlCode = pathParameters.replace("/", "");
+        if (shortUrlCode.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input: 'shortUrlCode' is required.");
+        }
+
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket("laryssa-url-shortener-storage-2024-x8q7zl")
+                .key(shortUrlCode + ".json")
+                .build();
+
+        UrlData urlData;
+        try (InputStream s3ObjectStream = s3Client.getObject(getObjectRequest)) {
+            urlData = objectMapper.readValue(s3ObjectStream, UrlData.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Error fetching or deserializing data from S3: " + e.getMessage(), e);
+        }
+
+        long currentTimeInSeconds = System.currentTimeMillis() / 1000;
+        if (urlData.getExpirationTime() < currentTimeInSeconds) {
+            response.put("statusCode", 410);
+            response.put("body", "This URL has expired");
+            return response;
+        }
+
+        response.put("statusCode", 302);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Location", urlData.getOriginalUrl());
+        response.put("headers", headers);
+
+        return response;
+    }
+}
